6eb84a27ae2160b06df1026008e4471734443bcb
WENDPANGA1 <w.ima@alustudent.com>
2023-07-16 18:59:11 +0000

    0. How many subs?

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
-""" 0-main """
+"""
+Write a function that queries the Reddit API and returns the number of subscribers.
+"""
+
 import requests
 
-def number_of_subscribers(subreddit):
-    """ number_of_subscribers """
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(url, headers=headers)
 
-    if response.status_code == 200:
-    return response.json()["data"]["subscribers"]
-    else:
-    return 0
+def number_of_subscribers(subreddit):
+    """
+        of subscribers for a given subreddit
+    """
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
+        return 0
+    data = response.json().get('data')
+    return data.get('subscribers')
